5c2b5288572a06b9018da85e149fbecf3f7e3cc7
Gaurav Sharma <gau1711sha@gmail.com>
2025-05-09 22:49:59 +0530

    submission controllers completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 import cookieParser from 'cookie-parser'
 
-import authRoute from './routes/auth.routes.js'
+import authRoutes from './routes/auth.routes.js'
 import problemRoutes from './routes/problem.routes.js'
 import executionRoute from './routes/executeCode.routes.js'
+import submissionRoutes from './routes/submission.routes.js'
 
 const app  = express();
 
@@ -18,9 +19,11 @@ app.get('/',(req,res)=>{
     res.send("hello leetlab🔥")
 })
 
-app.use('/api/v1/auth',authRoute)
+app.use('/api/v1/auth',authRoutes)
 app.use('/api/v1/problems',problemRoutes);
 app.use('/api/v1/execute',executionRoute);
+app.use('/api/v1/submission',submissionRoutes);
+
 
 app.listen(PORT,()=>{
 console.log(`server is listning to ${PORT}`)
